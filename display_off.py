5ce9c4fa0442a5ccfe7b1b8a91c560f7d537e178
Paul Smith <paul@psmith.me>
2020-12-10 06:37:27 -0700

    Initial commit

---
+++ display_off.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Import Blinka
+from board import SCL, SDA
+import busio
+import adafruit_ssd1306
+
+# Create the I2C interface.
+i2c = busio.I2C(SCL, SDA)
+
+# Create the SSD1306 OLED class.
+# The first two parameters are the pixel width and pixel height.  Change these
+# to the right size for your display!
+disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+
+# Clear display.
+disp.fill(0)
+disp.show()
+
